87407fc438fb192faaceb85bd1743846f7be78f0
Abdurahim <89141756+pxls00@users.noreply.github.com>
2022-07-15 10:27:26 +0500

    The  FORM component of this repo

--- components/form.php
+++ components/form.php
@@ -2,9 +2,11 @@
     echo "
     <div class='form'>
     <h2 class='form__title'>~ Today I need to ~</h2>
-    <form>
-      <input type='text' placeholder='Add new todo..' />
-      <button class='create-todo'>
+    <form action='/vendor/create.php' method='post'>
+      <input type='text' name='title' placeholder='Your product title...' maxlength='80' required/>
+      <input type='number' name='price' placeholder='Your product price...' maxlength='8' required/>
+      <textarea class='description'  name='description'></textarea>
+      <button class='create-todo' name='sendTask'>
         <span>Submit</span>
       </button>
     </form>
